fe548ae31824fe43f0c4833910d8d9d7fb8b866e
michelyouss <michel.youssef@edu.ece.fr>
2023-11-23 16:20:18 +0100

    initial commit

---
+++ Project-Data-Analytics.R
@@ -0,0 +1,36 @@
+#DataCollection
+uber_data_april = read.csv("DataSets_uber-pickups-in-new-york-city/uber-raw-data-apr14.csv")
+uber_data_april
+summary(uber_data_april)
+str(uber_data_april)
+
+uber_data_aug = read.csv("DataSets_uber-pickups-in-new-york-city/uber-raw-data-aug14.csv")
+uber_data_aug
+summary(uber_data_aug)
+str(uber_data_aug)
+
+uber_data_july = read.csv("DataSets_uber-pickups-in-new-york-city/uber-raw-data-jul14.csv")
+uber_data_july
+summary(uber_data_july)
+str(uber_data_july)
+
+uber_data_june = read.csv("DataSets_uber-pickups-in-new-york-city/uber-raw-data-jun14.csv")
+uber_data_june
+summary(uber_data_june)
+str(uber_data_june)
+
+uber_data_may = read.csv("DataSets_uber-pickups-in-new-york-city/uber-raw-data-may14.csv")
+uber_data_may
+summary(uber_data_may)
+str(uber_data_may)
+
+uber_data_sep = read.csv("DataSets_uber-pickups-in-new-york-city/uber-raw-data-sep14.csv")
+uber_data_sep
+summary(uber_data_sep)
+str(uber_data_sep)
+
+
+uber_data_janjune = read.csv("DataSets_uber-pickups-in-new-york-city/uber-raw-data-janjune-15.csv")
+uber_data_janjune
+summary(uber_data_janjune)
+str(uber_data_janjune)
