d9d3376966d515c1403f2df09a70c1c70ea6cc54
michelyous <101189385+michelyous@users.noreply.github.com>
2023-11-27 17:29:55 +0100

    Update Project-Data-Analytics.R
    quick fix

--- Project-Data-Analytics.R
+++ Project-Data-Analytics.R
@@ -1,35 +1,35 @@
 #STEP 1: DataCollection
-uber_data_april = read.csv("DataSets_uber-pickups-in-new-york-city/uber-raw-data-apr14.csv")
+uber_data_april = read.csv("uber-raw-data-apr14.csv")
 uber_data_april
 summary(uber_data_april)
 str(uber_data_april)
 
-uber_data_aug = read.csv("DataSets_uber-pickups-in-new-york-city/uber-raw-data-aug14.csv")
+uber_data_aug = read.csv("uber-raw-data-aug14.csv")
 uber_data_aug
 summary(uber_data_aug)
 str(uber_data_aug)
 
-uber_data_july = read.csv("DataSets_uber-pickups-in-new-york-city/uber-raw-data-jul14.csv")
+uber_data_july = read.csv("uber-raw-data-jul14.csv")
 uber_data_july
 summary(uber_data_july)
 str(uber_data_july)
 
-uber_data_june = read.csv("DataSets_uber-pickups-in-new-york-city/uber-raw-data-jun14.csv")
+uber_data_june = read.csv("uber-raw-data-jun14.csv")
 uber_data_june
 summary(uber_data_june)
 str(uber_data_june)
 
-uber_data_may = read.csv("DataSets_uber-pickups-in-new-york-city/uber-raw-data-may14.csv")
+uber_data_may = read.csv("uber-raw-data-may14.csv")
 uber_data_may
 summary(uber_data_may)
 str(uber_data_may)
 
-uber_data_sep = read.csv("DataSets_uber-pickups-in-new-york-city/uber-raw-data-sep14.csv")
+uber_data_sep = read.csv("uber-raw-data-sep14.csv")
 uber_data_sep
 summary(uber_data_sep)
 str(uber_data_sep)
 
-uber_data_janjune = read.csv("DataSets_uber-pickups-in-new-york-city/uber-raw-data-janjune-15.csv")
+uber_data_janjune = read.csv("uber-raw-data-janjune-15.csv")
 uber_data_janjune
 summary(uber_data_janjune)
 str(uber_data_janjune)
@@ -37,7 +37,7 @@ str(uber_data_janjune)
 #STEP 2: Data Cleaning
 
 # Combine all data except janjune because janjune has different and additional column
-uber_data_2014 <- rbind(uber_data_april, uber_data_aug, uber_data_july,uber_data_june,uber_data_may,uber_data_sep)
+uber_data_14 <- rbind(uber_data_april, uber_data_aug, uber_data_july,uber_data_june,uber_data_may,uber_data_sep)
 
 # Make sure that all the date is combined
 cat("The dimensions of all data are:", dim(uber_data_2014))
@@ -142,4 +142,3 @@ barplot(monthly_rides, main = "Monthly Number of Rides", xlab = "Month", ylab = 
 
 #visualizing peak hours 
 plot(rides_per_hour, type = "o", col = "orange", xlab = "Hour of the Day", ylab = "Number of Rides", main = "Peak Hours Analysis")
-
