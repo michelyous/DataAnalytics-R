9b064028aabf60d21ac9a10eb4e47fa738a86177
Assia Fellague <109692780+assiafellague2001@users.noreply.github.com>
2023-12-04 21:38:39 +0100

    Update Project-Data-Analytics.R

--- Project-Data-Analytics.R
+++ Project-Data-Analytics.R
@@ -1,53 +1,52 @@
-#STEP 1: DataCollection
-uber_data_april = read.csv("uber-raw-data-apr14.csv")
+#DataCollection
+uber_data_april = read.csv("C:/Users/dell/Downloads/UberDatasets/uber-raw-data-apr14.csv")
 uber_data_april
 summary(uber_data_april)
 str(uber_data_april)
 
-uber_data_aug = read.csv("uber-raw-data-aug14.csv")
+uber_data_aug = read.csv("C:/Users/dell/Downloads/UberDatasets/uber-raw-data-aug14.csv")
 uber_data_aug
 summary(uber_data_aug)
 str(uber_data_aug)
 
-uber_data_july = read.csv("uber-raw-data-jul14.csv")
+uber_data_july = read.csv("C:/Users/dell/Downloads/UberDatasets/uber-raw-data-jul14.csv")
 uber_data_july
 summary(uber_data_july)
 str(uber_data_july)
 
-uber_data_june = read.csv("uber-raw-data-jun14.csv")
+uber_data_june = read.csv("C:/Users/dell/Downloads/UberDatasets/uber-raw-data-jun14.csv")
 uber_data_june
 summary(uber_data_june)
 str(uber_data_june)
 
-uber_data_may = read.csv("uber-raw-data-may14.csv")
+uber_data_may = read.csv("C:/Users/dell/Downloads/UberDatasets/uber-raw-data-may14.csv")
 uber_data_may
 summary(uber_data_may)
 str(uber_data_may)
 
-uber_data_sep = read.csv("uber-raw-data-sep14.csv")
+uber_data_sep = read.csv("C:/Users/dell/Downloads/UberDatasets/uber-raw-data-sep14.csv")
 uber_data_sep
 summary(uber_data_sep)
 str(uber_data_sep)
 
-uber_data_janjune = read.csv("uber-raw-data-janjune-15.csv")
+uber_data_janjune = read.csv("C:/Users/dell/Downloads/DataSets_uber-pickups-in-new-york-city/uber-raw-data-janjune-15.csv")
 uber_data_janjune
 summary(uber_data_janjune)
 str(uber_data_janjune)
 
-#STEP 2: Data Cleaning
-
-# Combine all data except janjune because janjune has different and additional column
-uber_data_14 <- rbind(uber_data_april, uber_data_aug, uber_data_july,uber_data_june,uber_data_may,uber_data_sep)
-
+# Combine All the data together  
+uber_data_2014 <- rbind(uber_data_april, uber_data_aug, uber_data_july,uber_data_june,uber_data_may,uber_data_sep)
+uber_data_2014
 # Make sure that all the date is combined
 cat("The dimensions of all data are:", dim(uber_data_2014))
 
-#CLEANING DATA: CHECK FOR UNIQUENESS AND NAN_VALUES
+#CLEANING DATA: CHECK FOR UNIQUENESS AND NAN-VALUES
 
-#NAN VALUES
+#----------------------------------------------------------------------------------
+#NAN-values
 
 # Check for missing values in each column
-missing_values <- colSums(is.na(uber_data_14))
+missing_values <- colSums(is.na(uber_data_2014))
 
 # Display the count of missing values in each column
 print("Missing values in each column:")
@@ -55,12 +54,14 @@ print(missing_values)
 
 #Since there is no missing value we do not change anything
 
+#-----------------------------------------------------------------------------------
 #UNIQUENESS
-#Identify duplicate rows
-duplicated_rows <- duplicated(uber_data_14)
+# Identify duplicate rows
+duplicated_rows <- duplicated(uber_data_214)
 
 # Count the number of duplicate rows
 num_duplicates <- sum(duplicated_rows)
+
 
 # Check if there are any duplicate rows
 if (num_duplicates > 0) {
@@ -74,10 +75,10 @@ if (num_duplicates > 0) {
 }
 
 # Remove duplicated rows
-uber_data_14_cleaned <- unique(uber_data_14)
+uber_data_2014_cleaned <- unique(uber_data_2014)
 
 # Identify duplicate rows
-duplicated_rows <- duplicated(uber_data_14_cleaned)
+duplicated_rows <- duplicated(uber_data_2014_cleaned)
 
 # Count the number of duplicate rows
 num_duplicates <- sum(duplicated_rows)
@@ -92,49 +93,30 @@ if (num_duplicates > 0) {
   print("No duplicate rows.")
 }
 
-#Average Passenger Determination:
+#3 data analysis
 
-#Ensure that the 'date' column is of type Date
-uber_data_14_cleaned$Date.Time <- as.Date(uber_data_14_cleaned$Date.Time)
+par(mfrow=c(1,1))
 
-# Calculate the daily count of Uber trips using the table function
-# table: Count the occurrences of each unique date in the 'date' column
-daily_trip_counts <- table(uber_data_14_cleaned$Date.Time)
-
-# Create a new dataframe with the results
-# names(daily_trip_counts): Extract the unique dates from the result of table
-# as.numeric(): Convert the count values to numeric
-# date: The unique dates
-# trip_counts: The count of Uber trips for each date
-result_data <- data.frame(date = as.Date(names(daily_trip_counts)), trip_counts = as.numeric(daily_trip_counts))
-
-# Display the results
-print(result_data)
-summary(result_data)
-str(result_data)
-
-#STEP 3: Data Analysis
-
-head(uber_data_april)
-uber_data_april$Date.Time <- as.POSIXct(uber_data_april$Date.Time, format="%d/%m/%Y %H:%M:%S")
+head(uber_data_2014)
+uber_data_2014$Date.Time <- as.POSIXct(uber_data_2014$Date.Time, format="%d/%m/%Y %H:%M:%S")
 
 #daily number of rides : 
 uber_data_april$day <- format(uber_data_april$Date.Time, "%Y-%m-%d")
 daily_rides <- table(uber_data_april$day)
-daily_rides
+length(daily_rides)
 
 #monthly number of rides
-uber_data_april$month <- format(uber_data_april$Date.Time, "%Y-%m")
-monthly_rides <- table(uber_data_april$month)
-monthly_rides
+uber_data_2014$month <- format(uber_data_2014$Date.Time, "%Y-%m")
+monthly_rides <- table(uber_data_2014$month)
+length(monthly_rides)
 
-#peak hours  
-uber_data_april$hour <- as.numeric(format(uber_data_april$Date.Time, "%H"))
-rides_per_hour <- table(uber_data_april$hour)
+#peak hours 
+uber_data_2014$hour <- as.numeric(format(uber_data_2014$Date.Time, "%H"))
+rides_per_hour <- table(uber_data_2014$hour)
+rides_per_hour
 
-#Step 4: Data Visualization
 
-#visualizing daily number of rides 
+#visualizing daily number of rides for month April (example)
 barplot(daily_rides, main = "Daily Number of Rides", xlab = "Date", ylab = "Number of Rides", col = "skyblue")
 
 #visualizing monthly number of rides 
@@ -142,3 +124,46 @@ barplot(monthly_rides, main = "Monthly Number of Rides", xlab = "Month", ylab = 
 
 #visualizing peak hours 
 plot(rides_per_hour, type = "o", col = "orange", xlab = "Hour of the Day", ylab = "Number of Rides", main = "Peak Hours Analysis")
+
+#information extraction 
+
+
+#Association and Dimensioning using Apriori algorithm 
+install.packages("arules")
+library(arules)
+
+uber_data_2014
+
+transactions <- as(uber_data_2014[, c("month", "hour", "Lat", "Lon", "Base")], "transactions")
+
+rules <- apriori(transactions, parameter = list(support = 0.001, confidence = 0.6))
+
+inspect(rules)
+rules
+
+# from the association rules : 
+
+# *Certain hours like : 12h and 18h 00h are frequently associated with a certain locations such as
+# Lon=[-73.97,-72.07]} => Lat=[40.8,42.1] 
+# 
+# *certain hours like : 12h, 18h and 00h are associated with certain bases such as : Base=B02512,  
+# Base=B02682, Base=B02598.
+# 
+# *at 00h, the actif bases are : Base=B02764, Base=B02617, Base=B02512
+# 
+# *Peak horus for the following months : 04, 03, 02, 08, 05, 11, 10, 09 is 12h and 18h 
+# while for the following months : 06, 02, 05 is 00h and 12h.
+
+quality(rules)$support
+quality(rules)$confidence
+
+#ploting the association rules scatterplot
+plot(quality(rules)$support, quality(rules)$confidence, xlab = "Support", ylab = "Confidence", col = "blue", main = "Association Rules Scatterplot")
+#Interpreting the plot : most of the rules are concentrated in he upper left of the plot 
+
+#Prediction model :
+uber_data_2014
+
+
+
+
